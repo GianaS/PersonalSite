53dabbb946b54aaecd9aac7fff546418939a1095
Giana Solviletti <gianasolviletti@MacBook-Pro.local>
2020-06-21 15:10:25 -0400

    removed trailing slash with plugin

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
-<p align='center'>
-  <a href='https://www.gatsbyjs.org'>
-    <img alt='Gatsby' src='./images/lemon-emoji.png' width='60' />
-  </a>
-</p>
 <h1 align='center'>
   Coming Soon
 </h1>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,6 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
-import { Image } from 'semantic-ui-react'
 
 const Wrapper = styled.div`
   padding: 30px;
